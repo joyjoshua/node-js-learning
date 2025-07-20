38c2cef5b8270863c13c579a0215cfc457500df3
joyjoshua <125115930+joyjoshua@users.noreply.github.com>
2025-07-20 20:20:48 +0530

    namaste node ep5 notes added and changed folder structure

--- code_snippets/app.js
+++ code_snippets/app.js
@@ -9,10 +9,11 @@
 //Namaste Node Ep 4
 
 // const {calculateSum, SUM_KEY} = require('./modules/calculate/sum');
-const {calculateSum, calculateProduct} = require('./modules/calculate')
+const {calculateSum, calculateProduct, fileName, directoryName} = require('./modules/calculate')
 const userData = require('./data.json');
 console.log('APP MODULE RUNNING');
-
+console.log(fileName);
+console.log(directoryName);
 var a = 10;
 var b = 20;
 
@@ -20,3 +21,25 @@ calculateSum(a, b);
 calculateProduct(a, b);
 console.log("User Data:", userData);
 // console.log('key from sum.js:',SUM_KEY);
+
+//IIFE
+
+(function(){
+    console.log('Immediately Invoked Function, hell yeah bro!');
+})();
+
+console.log((function(){console.log('IIFE')})) 
+
+// function modules() {
+//     let z = 20;
+
+//     function b () {
+//         console.log('b');
+//     }
+//     b();
+// }
+
+// console.log(z);
+
+// modules();
+
--- code_snippets/modules/calculate/index.js
+++ code_snippets/modules/calculate/index.js
@@ -1,4 +1,4 @@
 const {calculateSum} = require('./sum');
 const {calculateProduct} = require('./multiply');
 
-module.exports = {calculateSum, calculateProduct};
+module.exports = {calculateSum, calculateProduct, fileName: __filename, directoryName: __dirname};
