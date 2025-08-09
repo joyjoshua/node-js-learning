c6881563758161c96f3ed871fdf1a323a52a50be
joyjoshua <125115930+joyjoshua@users.noreply.github.com>
2025-08-09 22:06:46 +0530

    added grouping, bulk operations and find-replace collection methods notes and code

--- code_snippets/database.js
+++ code_snippets/database.js
@@ -132,7 +132,95 @@ async function main() {
     ];
     const aggs = await collection.aggregate(pipeline).toArray();
     console.log('aggs res:', aggs);
-    
+
+    //grouping methods
+
+    //createIndex
+    const newIndex = await collection.createIndex({lorem_ipsum: 1});
+    console.log('new index added', newIndex);
+
+    //dropIndexes
+    const removeIndex = await collection.dropIndex('lorem_ipsum_1');
+    console.log('sorry man gotta drop you', removeIndex);
+
+    //indexInformation
+    const indexInfo = await collection.indexInformation();
+    console.log('index info', indexInfo);
+
+    //indexExists
+    const doesIdExist = await collection.indexExists('_id_');
+    console.log('DO I EXIST?', doesIdExist);
+
+    //listIndexes
+    const listIndexes = await collection.listIndexes();
+    console.log('List of indexes', listIndexes);
+
+    //counting and estimations
+    //estimatedDocumentCount
+    const docuCounts = await collection.estimatedDocumentCount();
+    console.log('another count method', docuCounts);
+
+    //bulk ops
+    //bulkWrite
+    const ops = [
+        {
+            insertOne: {
+                document: {
+                        first_name: 'bulk',
+                        last_name: 'bro',
+                        city: 'Bengaluru',
+                        hyper_level: '100',
+                }
+            }
+        },
+        {
+            updateOne: {
+                filter: {first_name: 'j'},
+                update: {
+                    $set: {
+                        hyper_level: 'Infinity + 1'
+                    }
+                }
+            }
+        },
+        {
+            deleteOne: {
+                filter: {first_name: 'hermoine'}
+            }
+        },
+    ];
+
+    const bulkWrite = await collection.bulkWrite(ops);
+
+    console.log("Inserted count:", bulkWrite.insertedCount);
+    console.log("Modified count:", bulkWrite.modifiedCount);
+    console.log("Deleted count:", bulkWrite.deletedCount);
+
+
+    //find and modify
+    //findOneAndUpdate
+    const info = {
+        $set: {
+            last_name: 'cub3',
+        },
+    };
+    const findOneAndUpdateIt = await collection.findOneAndUpdate({first_name: 'j'}, info);
+    console.log('found one guy and updated his last name', findOneAndUpdateIt);
+
+    //findOneAndReplace
+    const hankyPankyDoc = {
+        first_name: 'hank',
+        last_name: 'schrader',
+        city: 'New Mexico',
+        hyper_level: '-1',
+    }
+    const replaceBulkBro = await collection.findOneAndReplace({first_name: 'bulk'}, hankyPankyDoc);
+    console.log('bulk bro got replaced', replaceBulkBro);
+
+    //findOneAndDelete
+    const deleteHank = await collection.findOneAndDelete({first_name: 'hank'});
+    console.log('Sorry hank you got deleted by Jack', deleteHank);
+
     return 'done';
 }
 
